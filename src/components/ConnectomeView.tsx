1db473e2b93d47a7b44f2e555a9f02b68d7fc414
Albert Alises <albert@mint-labs.com>
2018-02-07 17:37:53 +0100

    Implemented filtering on the chart by thresholding (threshold is part of the state), and indenting

--- src/components/ConnectomeView.tsx
+++ src/components/ConnectomeView.tsx
@@ -1,10 +1,9 @@
-import * as React from 'react'
-import { ResponsiveChord } from '@nivo/chord'
-import * as Papa from 'papaparse';
-import {stylesheet} from 'react-stylesheet-decorator'
+import * as React from "react";
+import { ResponsiveChord } from "@nivo/chord";
+import * as Papa from "papaparse";
+import { stylesheet } from "react-stylesheet-decorator";
 
 export default class ConnectomeView extends React.Component {
-
   state = {
     connectomeData: [], //Matrix with all the data
     currentData: [], //Current filtered matrix
@@ -12,105 +11,131 @@ export default class ConnectomeView extends React.Component {
     //Header Data [Labels, Regions]
     labels: [],
     groups: [],
+    threshold: 0.12, //Threshold of the data. The connections whose value are < won't be shown
     showChord: true
-  }
+  };
 
   //Groups of our connectome ordered by preference (clockwise starting from the top) on the chord diagram. Data pertaining to a group not here will be placed first
-  groups = ["Right","Subcortical-Right","Brainstem","Subcortical-Left","Left"];
+  groups = [
+    "Right",
+    "Subcortical-Right",
+    "Brainstem",
+    "Subcortical-Left",
+    "Left"
+  ];
 
   //Util functions for getting a column of an array and transposing an array
   arrayColumn = (arr, n) => arr.map(x => x[n]);
-  transpose = (data) => data[0].map((col, i) => data.map(row => row[i]));
+  transpose = data => data[0].map((col, i) => data.map(row => row[i]));
 
   private toggleDiagrams() {
     this.setState({ showChord: !this.state.showChord });
   }
 
-  private parseData(){
-    Promise.all([this.parseLabelInfo(),this.parseDataFromCSV()]).then(_ =>{
+  private parseData() {
+    Promise.all([this.parseLabelInfo(), this.parseDataFromCSV()]).then(_ => {
       this.sortData(); //Sort the data once everything is loaded
+      var filtData = this.filterDataThres(
+        this.state.connectomeData.slice(),
+        this.state.threshold
+      );
+      this.setState({ currentData: filtData });
     });
   }
 
-  private groupsorting = (a, b) => { //Note: Reorders rows of the matrix
+  private groupsorting = (a, b) => {
+    //Note: Reorders rows of the matrix
     var orientA = this.groups.indexOf(a[0]);
     var orientB = this.groups.indexOf(b[0]);
 
-    return (orientA == orientB) ? 0 : (orientA < orientB) ? -1 : 1; 
+    return orientA == orientB ? 0 : orientA < orientB ? -1 : 1;
+  };
+
+  private filterDataThres(data, thres) {
+    var filteredData = data.map(row => {
+      return row.map(el => {
+        return el < thres ? 0 : el;
+      });
+    });
+    return filteredData;
   }
 
-  /* 
-    Sorts the data anatomically based on the groups
-  */
-  private sortData(){ 
-
+  /* Sorts the data anatomically based on the groups */
+  private sortData() {
     var groups = this.state.groups.slice();
     var labels = this.state.labels.slice();
     var data = this.state.connectomeData.slice();
-    data.unshift(groups,labels);
+    data.unshift(groups, labels);
 
     /* Sort the columns of the matrix and set the labels and regions */
     data = this.transpose(data);
     data.sort(this.groupsorting);
-    this.setState({groups : this.arrayColumn(data,0)});
-    this.setState({labels : this.arrayColumn(data,1)});
+    this.setState({ groups: this.arrayColumn(data, 0) });
+    this.setState({ labels: this.arrayColumn(data, 1) });
     data = this.transpose(data);
 
     /* Sort the rows of the matrix the same way to keep the matrix symmetric */
     //First re-add the original labels and regions on the transposed matrix to do the row sorting
-    
-    data = this.transpose(data.splice(2,data.length));
-    data.unshift(groups,labels);
+
+    data = this.transpose(data.splice(2, data.length));
+    data.unshift(groups, labels);
 
     data = this.transpose(data); //At this point we have the original matrix with the two first columns our header
     data.sort(this.groupsorting);
     data = this.transpose(data);
 
     //Erase the labels and groups from the data
-    data = this.transpose(data.splice(2,data.length));
+    data = this.transpose(data.splice(2, data.length));
 
-    this.setState({connectomeData: data});
-    this.setState({currentData: data});
-
+    this.setState({ connectomeData: data });
   }
 
-  private parseDataFromCSV(){
-    return new Promise((resolve,reject) => {
-      Papa.parse("https://raw.githubusercontent.com/aalises/ami-viewerData/master/connectome.csv", {
-        download: true,
-        dynamicTyping: true,
-        skipEmptyLines: true,
-        complete: (resultData) => {
-          this.setState({ connectomeData: resultData.data});
-          resolve();
+  private parseDataFromCSV() {
+    return new Promise((resolve, reject) => {
+      Papa.parse(
+        "https://raw.githubusercontent.com/aalises/ami-viewerData/master/connectome.csv",
+        {
+          download: true,
+          dynamicTyping: true,
+          skipEmptyLines: true,
+          complete: resultData => {
+            this.setState({ connectomeData: resultData.data });
+            resolve();
+          }
         }
-      });
+      );
     });
   }
 
-  private parseLabelInfo(){
-    return new Promise((resolve,reject) => {
-      Papa.parse("https://raw.githubusercontent.com/aalises/ami-viewerData/master/volumetric.csv", {
-        download: true,
-        dynamicTyping: true,
-        skipEmptyLines: true,
+  private parseLabelInfo() {
+    return new Promise((resolve, reject) => {
+      Papa.parse(
+        "https://raw.githubusercontent.com/aalises/ami-viewerData/master/volumetric.csv",
+        {
+          download: true,
+          dynamicTyping: true,
+          skipEmptyLines: true,
 
-        complete: (resultHeader) => {
-          var numResidualRows = 9; //On the volumetric CSV, the unused labels are the first 9 rows
-          var filteredArray = resultHeader.data.splice(numResidualRows,resultHeader.data.length - numResidualRows);
-          this.setState({ labels: this.arrayColumn(filteredArray,8)});
-          this.setState({ groups: this.arrayColumn(filteredArray,9)});
-          resolve();
+          complete: resultHeader => {
+            var numResidualRows = 9; //On the volumetric CSV, the unused labels are the first 9 rows
+            var filteredArray = resultHeader.data.splice(
+              numResidualRows,
+              resultHeader.data.length - numResidualRows
+            );
+            this.setState({ labels: this.arrayColumn(filteredArray, 8) });
+            this.setState({ groups: this.arrayColumn(filteredArray, 9) });
+            resolve();
+          }
         }
-      });
+      );
     });
   }
 
-    componentDidMount() {
-      this.parseData();
-    }
+  componentDidMount() {
+    this.parseData();
+  }
 
-    @stylesheet(`
+  @stylesheet(`
 
     .connectomeview {
       position: fixed;
@@ -136,20 +161,22 @@ export default class ConnectomeView extends React.Component {
       display: flex;
     }
   `)
+  public render() {
+    const dataReady = this.state.labels.length && this.state.currentData.length;
 
-    public render(){
-      return (
-        <div className="connectomeview">
-        {this.state.labels.length && this.state.currentData.length &&
-            //Render the chord Diagram
-            <ResponsiveChord
+    return (
+      <div className="connectomeview">
+        {!dataReady && <div> Data is loading </div>}
+        {dataReady && (
+          //Render the chord Diagram
+          <ResponsiveChord
             matrix={this.state.currentData}
             keys={this.state.labels}
             margin={{
-                "top": 0,
-                "right": 300,
-                "bottom": 40,
-                "left": 300
+              top: 0,
+              right: 300,
+              bottom: 40,
+              left: 300
             }}
             pixelRatio={1}
             padAngle={0.03}
@@ -173,11 +200,9 @@ export default class ConnectomeView extends React.Component {
             ribbonHoverOpacity={0.9}
             ribbonHoverOthersOpacity={0.1}
             animate={false}
-        />
-        }
-        </div>
-      );
-    }
-  
+          />
+        )}
+      </div>
+    );
   }
-
+}
